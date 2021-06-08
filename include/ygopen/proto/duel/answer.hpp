3f466fea3fb2e8e718515b72e2975416cbef8294
Dylam De La Torre <dyxel04@gmail.com>
2021-06-08 21:15:25 +0200

    add placeholder Duel.Answer message

---
+++ include/ygopen/proto/duel/answer.hpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Dylam De La Torre <dyxel04@gmail.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+#ifndef YGOPEN_PROTO_DUEL_ANSWER_HPP
+#define YGOPEN_PROTO_DUEL_ANSWER_HPP
+// clang-format off
+#include "data.hpp"
+#include <duel_answer.pb.h>
+// clang-format on
+
+#endif // YGOPEN_PROTO_DUEL_ANSWER_HPP
