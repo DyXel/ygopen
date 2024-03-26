fcd5d1a946477c26ea0a774a8e1aced433188d19
Dylam De La Torre <dyxel04@gmail.com>
2024-03-26 18:46:27 +0100

    add client_msg and server_msg protos and headers

---
+++ include/ygopen/proto/client_msg.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024, Dylam De La Torre <dyxel04@gmail.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+#ifndef YGOPEN_PROTO_CLIENT_MSG_HPP
+#define YGOPEN_PROTO_CLIENT_MSG_HPP
+// clang-format off
+#include <net_client_msg.pb.h>
+// clang-format on
+
+namespace YGOpen::Proto
+{
+
+} // namespace YGOpen::Proto
+
+#endif // YGOPEN_PROTO_CLIENT_MSG_HPP
